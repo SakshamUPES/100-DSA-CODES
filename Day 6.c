0578015e4a1db59abaec1bfd56de19bf1b83c121
Saksham Saxena <sakshamsaxena9410@gmail.com>
2026-02-07 11:25:03 +0530

    Day 6.c

--- Day 6.c
+++ Day 6.c
@@ -1,1 +1,35 @@
-r
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);  
+    
+    int arr[100];    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  
+    }
+    
+    if(n == 0) {
+        printf("\n");
+        return 0;
+    }
+    
+
+    int k = 1;  
+    
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[i-1]) { 
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+    
+
+    for(int i = 0; i < k; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
