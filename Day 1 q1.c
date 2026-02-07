c16b11a2b9bff7e7b78fa8192bf780e6273b85ed
Saksham Saxena <sakshamsaxena9410@gmail.com>
2026-02-07 11:05:52 +0530

    Day 1 q1.c

--- Day 1 q1.c
+++ Day 1 q1.c
@@ -1,1 +1,31 @@
-d
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n); 
+    
+    int arr[100];     
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]); 
+    }
+    
+    int pos, x;
+    scanf("%d", &pos); 
+    scanf("%d", &x);   
+    
+    for(int i = n; i > pos-1; i--) {
+        arr[i] = arr[i-1];
+    }
+    
+    arr[pos-1] = x;  
+    n++;           
+    
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
