a3b1fb1b84e13e1ac0e3b2a15a5fea74aa45b91a
Saksham Saxena <sakshamsaxena9410@gmail.com>
2026-02-21 10:23:00 +0530

    Day-8 .c

--- Day-8 .c
+++ Day-8 .c
@@ -1,1 +1,21 @@
-s
+#include <stdio.h>
+
+
+int power(int a, int b) {
+    if (b == 0)
+        return 1;        
+    else
+        return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+
+    // Input
+    scanf("%d %d", &a, &b);
+
+    // Output
+    printf("%d", power(a, b));
+
+    return 0;
+}
